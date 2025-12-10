1bb4758b3b48fb375888be67301e2724d5bb15fd
rydamandrohitshow-design <rydamandrohitshow@gmail.com>
2025-12-09 21:00:50 -0600

    Update uv.sw.js

--- uv.sw.js
+++ uv.sw.js
@@ -1,3 +1,1 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2/dist/uv.sw.js');
-const uv = new UltravioletServiceWorker();
-self.addEventListener('fetch', event => event.respondWith(uv.fetch(event.request)));
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2/dist/uv.sw.js'); const uv = new UltravioletServiceWorker(); self.addEventListener('fetch', e => e.respondWith(uv.fetch(e.request)));
