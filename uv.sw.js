a4f06493b8756f71f8a9d2bea4d3541e2b55887f
rydamandrohitshow-design <rydamandrohitshow@gmail.com>
2025-12-09 21:19:14 -0600

    Create uv.sw.js

---
+++ uv.sw.js
@@ -0,0 +1,1 @@
+importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2/dist/uv.sw.js'); const uv = new UltravioletServiceWorker(); self.addEventListener('fetch', e => e.respondWith(uv.fetch(e.request)));
